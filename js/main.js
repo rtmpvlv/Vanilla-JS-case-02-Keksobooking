84d15d51cde6c18e3304dd3eb6e21e50501e0396
Artem Pavlov <rtmpvlv@gmail.com>
2021-09-20 20:41:52 +0300

    Complited task three

--- js/main.js
+++ js/main.js
@@ -1,5 +1,44 @@
 'use strict';
 
+const SIMILAR_ADS_COUNT = 10;
+const PRISE = {
+  MIN: 1,
+  MAX: Infinity,
+};
+const ROOMS = {
+  MIN: 1,
+  MAX: Infinity,
+};
+const GUESTS = {
+  MIN: 1,
+  MAX: Infinity,
+};
+const TypeOfLiving = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+const CheckinTime = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const Features = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const Photos = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+
 const getRandomInteger = (min, max) => {
   if (max <= min) {
     [min, max] = [max, min];
@@ -19,6 +58,7 @@ const getRandomFloat = (min, max, lengthAfterThePoint) => {
 };
 
 const getRandomArrayElement = (elements) => elements[_.random(0, elements.length - 1)];
+
 const getNewRandomArray = (array) => {
   const newArray = array.slice();
   for (let i = newArray.length - 1; i > 0; i--) {
@@ -29,38 +69,11 @@ const getNewRandomArray = (array) => {
   return newArray;
 };
 
-const TYPE_OF_LIVING = [
-  'palace',
-  'flat',
-  'house',
-  'bungalow',
-];
-
-const CHECK_IN_TIME = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
-
-const FEATURES = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner',
-];
-
-const PHOTOS = [
-  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-];
-
-const randomLongitude = getRandomFloat(35.65000, 35.70000, 5);
-const randomLatitude = getRandomFloat(139.70000, 139.80000, 5);
-
 const getSimilarAd = function() {
+
+  const randomLongitude = getRandomFloat(35.65000, 35.70000, 5);
+  const randomLatitude = getRandomFloat(139.70000, 139.80000, 5);
+
   return {
     author: {
       avatar: 'img/avatars/user0' + getRandomInteger(1, 8) + '.png',
@@ -68,15 +81,15 @@ const getSimilarAd = function() {
     offer: {
       title: 'Отличное место для отдыха.',
       address: randomLongitude + ' ' + randomLatitude,
-      price: getRandomInteger(0, Infinity),
-      type: getRandomArrayElement(TYPE_OF_LIVING),
-      rooms: getRandomInteger(0, Infinity),
-      guests: getRandomInteger(0, Infinity),
-      checkin: getRandomArrayElement(CHECK_IN_TIME),
-      checkout: getRandomArrayElement(CHECK_IN_TIME),
-      features: getNewRandomArray(FEATURES),
+      price: getRandomInteger(PRISE.MIN, PRISE.MAX),
+      type: getRandomArrayElement(TypeOfLiving),
+      rooms: getRandomInteger(ROOMS.MIN, ROOMS.MAX),
+      guests: getRandomInteger(GUESTS.MIN, GUESTS.MAX),
+      checkin: getRandomArrayElement(CheckinTime),
+      checkout: getRandomArrayElement(CheckinTime),
+      features: getNewRandomArray(Features),
       description: 'Отличное помещение. Очень светлое. Много комнат. Отдых тут - просто красота.',
-      photos: getNewRandomArray(PHOTOS),
+      photos: getNewRandomArray(Photos),
     },
     location: {
       x: randomLongitude,
@@ -85,4 +98,4 @@ const getSimilarAd = function() {
   };
 };
 
-const similarAds = new Array(10).fill(null).map(() => getSimilarAd());
+const similarAds = new Array(SIMILAR_ADS_COUNT).fill(null).map(() => getSimilarAd());
